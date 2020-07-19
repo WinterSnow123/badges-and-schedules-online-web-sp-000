709f656bc17468c4200be27df5e261e0694e1576
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 16:28:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,20 @@ end
 def batch_badge_creator(speakers_array)
   speakers_array.collect do |speaker|
     badge_maker(speaker)
-end
+  end
 end
 
 def assign_rooms(speakers_array)
   rooms_array = []
   speakers_array.each_with_index do |speaker, index|
     rooms_array << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
-end
+  end
 rooms_array
+end
+
+def printer(speakers_array)
+  speakers_array.each do |speaker|
+    puts batch_badge_creator(speaker)
+    puts assign_rooms(speaker)
+  end
 end
